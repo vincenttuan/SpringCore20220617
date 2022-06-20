94da424b1570a793770663a9c219fe96c6e64c11
teacher <teacher@BE-403->
2022-06-20 19:52:14 +0800

    s

--- src/test/java/com/spring/core/session02/TestAuthor.java
+++ src/test/java/com/spring/core/session02/TestAuthor.java
@@ -13,12 +13,14 @@ public class TestAuthor {
 		ApplicationContext ctx = new ClassPathXmlApplicationContext("applicationContext.xml");
 		Author author1 = ctx.getBean("author1", Author.class);
 		System.out.println(author1);
+		// 進行手動注入
 		author1.setName("Vincent");
 		author1.setSex('M');
 		author1.setAge(30);
 		System.out.println(author1);
 		
 		Author author2 = ctx.getBean("author2", Author.class);
+		// author2 在配置檔中已經有預設注入資料(利用方法注入)
 		System.out.println(author2);
 	}
 }
