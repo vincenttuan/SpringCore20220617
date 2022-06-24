697fb42f2b3110b29a9fa5b766bc6d3911785813
teacher <teacher@BE-403->
2022-06-24 19:06:43 +0800

    s

--- src/test/java/com/spring/core/session03/TestClazz.java
+++ src/test/java/com/spring/core/session03/TestClazz.java
@@ -4,10 +4,13 @@ import org.junit.Test;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
+import com.spring.core.session03.beans.Clazz;
+
 public class TestClazz {
 	@Test
 	public void test() {
 		ApplicationContext ctx = new ClassPathXmlApplicationContext("applicationContext.xml");
-		
+		Clazz clazz = ctx.getBean("clazz1", Clazz.class);
+		System.out.println(clazz);
 	}
 }
