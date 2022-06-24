b5c532dfcd925970c048e453a664079d1313d384
teacher <teacher@BE-403->
2022-06-24 20:07:01 +0800

    s

--- src/test/java/com/spring/core/session03/TestTeacher.java
+++ src/test/java/com/spring/core/session03/TestTeacher.java
@@ -1,5 +1,7 @@
 package com.spring.core.session03;
 
+import java.util.IntSummaryStatistics;
+
 import org.junit.Test;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
@@ -14,6 +16,15 @@ public class TestTeacher {
 		Teacher teacher1 = ctx.getBean("teacher1", Teacher.class);
 		System.out.println(teacher1);
 		// 請問 teacher1 的學生每個人平均是幾學分 ?
-		
+		teacher1.getStudents().stream()
+			.map(s -> s.getClazzs())
+			.forEach(s -> {
+				int sum = s.stream().mapToInt(c -> c.getCredit()).sum();
+				double avg = s.stream().mapToInt(c -> c.getCredit()).average().getAsDouble();
+				System.out.println(sum + ", " + avg);
+				
+				IntSummaryStatistics stat = s.stream().mapToInt(c -> c.getCredit()).summaryStatistics();
+				System.out.println(stat);
+			});
 	}
 }
