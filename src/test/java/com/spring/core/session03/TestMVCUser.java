eeb67498d6ae5f7bb9e3bd4b5819ccd225ddf0f9
teacher <teacher@BE-403->
2022-06-27 19:27:16 +0800

    s

---
+++ src/test/java/com/spring/core/session03/TestMVCUser.java
@@ -0,0 +1,16 @@
+package com.spring.core.session03;
+
+import org.junit.Test;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+import com.spring.core.session03.mvc.model.User;
+
+public class TestMVCUser {
+	@Test
+	public void test() {
+		ApplicationContext ctx = new ClassPathXmlApplicationContext("applicationContext.xml");
+		User user = ctx.getBean("user", User.class);
+		System.out.println(user);
+	}
+}
