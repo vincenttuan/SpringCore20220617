9eeb9e9211563e1b109df00d3ca65a37f1151ddd
teacher <teacher@BE-403->
2022-06-27 21:24:18 +0800

    s

---
+++ src/test/java/com/spring/core/session04/ProxyStaticDemo.java
@@ -0,0 +1,19 @@
+package com.spring.core.session04;
+
+import org.junit.Test;
+
+import com.spring.core.session04.sta.Man;
+import com.spring.core.session04.sta.Person;
+import com.spring.core.session04.sta.Woman;
+
+public class ProxyStaticDemo {
+	@Test
+	public void test() {
+		// 不透過代理直接執行
+		Person man = new Man();
+		Person woman = new Woman();
+		man.work();
+		woman.work();
+		
+	}
+}
