d94c1566cc0bdaed5c238b366ed1859856409ca0
teacher <teacher@BE-403->
2022-06-27 19:57:04 +0800

    s

--- src/test/java/com/spring/core/session03/TestMVCUser.java
+++ src/test/java/com/spring/core/session03/TestMVCUser.java
@@ -4,6 +4,7 @@ import org.junit.Test;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
+import com.spring.core.session03.mvc.controller.UserController;
 import com.spring.core.session03.mvc.model.User;
 
 public class TestMVCUser {
@@ -12,5 +13,10 @@ public class TestMVCUser {
 		ApplicationContext ctx = new ClassPathXmlApplicationContext("applicationContext.xml");
 		User user = ctx.getBean("user", User.class);
 		System.out.println(user);
+		
+		// 將 user 加入到資料庫中
+		// UserController -> UserService -> UserDao
+		UserController userController = ctx.getBean("userController", UserController.class);
+		userController.appendUser(user);
 	}
 }
