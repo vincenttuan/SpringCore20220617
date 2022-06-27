506f36db12e47b786ef52c79e6c7847d12540ab5
teacher <teacher@BE-403->
2022-06-27 21:27:18 +0800

    s

--- src/main/java/com/spring/core/session04/sta/Woman.java
+++ src/main/java/com/spring/core/session04/sta/Woman.java
@@ -5,7 +5,7 @@ public class Woman implements Person {
 	@Override
 	public void work() {
 		// 公用邏輯
-		System.out.println("戴口罩");
+		//System.out.println("戴口罩");
 				
 		// 業務邏輯
 		System.out.println("出門買菜: 煮晚餐");
