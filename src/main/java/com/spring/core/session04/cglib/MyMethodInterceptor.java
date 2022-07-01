0aed201f51100cf098f61251fd003e64ac5b0793
teacher <teacher@BE-403->
2022-07-01 20:55:50 +0800

    s

---
+++ src/main/java/com/spring/core/session04/cglib/MyMethodInterceptor.java
@@ -0,0 +1,17 @@
+package com.spring.core.session04.cglib;
+
+import java.lang.reflect.Method;
+
+import org.springframework.cglib.proxy.MethodInterceptor;
+import org.springframework.cglib.proxy.MethodProxy;
+
+// 方法攔截器 (cglib 的使用)
+public class MyMethodInterceptor implements MethodInterceptor {
+
+	@Override
+	public Object intercept(Object obj, Method method, Object[] args, MethodProxy proxy) throws Throwable {
+		System.out.println("請出示實聯 ORCode");
+		return proxy.invokeSuper(obj, args);
+	}
+	
+}
