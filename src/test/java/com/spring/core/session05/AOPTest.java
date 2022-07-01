d236c384dfb5d97f02671c6200d24767c738c36c
teacher <teacher@BE-403->
2022-07-01 21:53:48 +0800

    s

--- src/test/java/com/spring/core/session05/AOPTest.java
+++ src/test/java/com/spring/core/session05/AOPTest.java
@@ -5,12 +5,13 @@ import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 import com.spring.core.session05.aop.MathCalc;
+import com.spring.core.session05.aop.MathCalcImpl;
 
 public class AOPTest {
 	@Test
 	public void test() {
 		ApplicationContext ctx = new ClassPathXmlApplicationContext("aop-config.xml");
-		MathCalc calc = ctx.getBean(MathCalc.class);
+		MathCalc calc = ctx.getBean("mathCalcImpl", MathCalcImpl.class);
 		System.out.println(calc.add(20, 10));
 	}
 }
