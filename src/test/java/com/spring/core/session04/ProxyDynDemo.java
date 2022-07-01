da8b588f1ffd3b9f0b154a5fb02f76c33943e075
teacher <teacher@BE-403->
2022-07-01 19:31:38 +0800

    s

--- src/test/java/com/spring/core/session04/ProxyDynDemo.java
+++ src/test/java/com/spring/core/session04/ProxyDynDemo.java
@@ -2,9 +2,15 @@ package com.spring.core.session04;
 
 import org.junit.Test;
 
+import com.spring.core.session04.dyn.Calc;
+import com.spring.core.session04.dyn.CalcImpl;
+import com.spring.core.session04.dyn.ProxyDynUtil;
+
 public class ProxyDynDemo {
 	@Test
 	public void test() {
-		
+		Calc calc = (Calc)new ProxyDynUtil(new CalcImpl()).getProxy();
+		System.out.println(calc.add(20, 10));
+		System.out.println(calc.div(20, 10));
 	}
 }
