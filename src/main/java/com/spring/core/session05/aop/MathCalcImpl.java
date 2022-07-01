4f09acc840930738d9ab82a0af2194e36d602927
teacher <teacher@BE-403->
2022-07-01 21:24:20 +0800

    s

---
+++ src/main/java/com/spring/core/session05/aop/MathCalcImpl.java
@@ -0,0 +1,17 @@
+package com.spring.core.session05.aop;
+
+public class MathCalcImpl implements MathCalc {
+
+	@Override
+	public Integer add(Integer x, Integer y) {
+		Integer result = x + y;
+		return result;
+	}
+
+	@Override
+	public Integer div(Integer x, Integer y) {
+		Integer result = x / y;
+		return result;
+	}
+
+}
