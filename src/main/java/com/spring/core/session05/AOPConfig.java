4e43c485cdac7c4be83be0dfc2fe7cf1c9d0bdf5
teacher <teacher@BE-403->
2022-07-04 21:16:48 +0800

    s

--- src/main/java/com/spring/core/session05/AOPConfig.java
+++ src/main/java/com/spring/core/session05/AOPConfig.java
@@ -1,8 +1,12 @@
 package com.spring.core.session05;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.EnableAspectJAutoProxy;
+
+import com.spring.core.session05.aop_lab.Dancer;
+import com.spring.core.session05.aop_lab.Performance;
 
 @Configuration // 此為 Java 配置檔
 //@ComponentScan(basePackages = {"com.spring.core.session04", "com.spring.core.session03"}) // 啟用元件掃描(指定掃瞄路徑)
@@ -10,4 +14,9 @@ import org.springframework.context.annotation.EnableAspectJAutoProxy;
 @EnableAspectJAutoProxy // 自動生成代理物件
 public class AOPConfig {
 	
+	@Bean(name = "dancer")
+	public Performance dancer() {
+		Dancer dancer = new Dancer();
+		return dancer;
+	}
 }
