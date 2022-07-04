0f56fbbff8630a6229c3155ee20bf64269d04560
teacher <teacher@BE-403->
2022-07-04 21:26:07 +0800

    s

--- src/main/java/com/spring/core/session05/aop_lab/Audience.java
+++ src/main/java/com/spring/core/session05/aop_lab/Audience.java
@@ -7,4 +7,25 @@ import org.aspectj.lang.annotation.Pointcut;
 public class Audience {
 	@Pointcut(value = "execution(* com.spring.core.session05.aop_lab.Performance.perform(..))")
 	public void pt() {}
+	
+	public void slienceCellPhone() {
+		System.out.println("觀眾-關閉手機");
+	}
+	
+	public void takeSeats() {
+		System.out.println("觀眾-找到座位");
+	}
+	
+	public void applause() {
+		System.out.println("觀眾-拍手鼓掌");
+	}
+	
+	public void exit() {
+		System.out.println("觀眾-離開表演會場");
+	}
+	
+	public void demanRefund() {
+		System.out.println("觀眾-退票 ! 退票 ! 並離場");
+	}
+	
 }
