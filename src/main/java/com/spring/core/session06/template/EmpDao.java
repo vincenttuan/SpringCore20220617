2fea6c0d668caa34af2c66dc0b496911a2651135
teacher <teacher@BE-403->
2022-07-11 20:32:09 +0800

    s

--- src/main/java/com/spring/core/session06/template/EmpDao.java
+++ src/main/java/com/spring/core/session06/template/EmpDao.java
@@ -1,11 +1,15 @@
 package com.spring.core.session06.template;
 
+import java.sql.ResultSet;
 import java.util.List;
 import java.util.Map;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.RowMapper;
 import org.springframework.stereotype.Repository;
+
+import com.spring.core.session06.entity.Emp;
 
 // Dao: Data Access Object
 @Repository
@@ -18,6 +22,21 @@ public class EmpDao {
 	public List<Map<String, Object>> queryAll() {
 		String sql = "select eid, ename, age, createtime from emp";
 		List<Map<String, Object>> emps = jdbcTemplate.queryForList(sql);
+		return emps;
+	}
+	
+	// 多筆查詢 II
+	public List<Emp> queryAllEmps() {
+		String sql = "select eid, ename, age, createtime from emp";
+		RowMapper<Emp> rowMapper = (ResultSet rs, int rowNum) -> {
+			Emp emp = new Emp();
+			emp.setEid(rs.getInt("eid"));
+			emp.setEname(rs.getString("ename"));
+			emp.setAge(rs.getInt("age"));
+			emp.setCreatetime(rs.getTimestamp("createtime"));
+			return emp;
+		};
+		List<Emp> emps = jdbcTemplate.query(sql, rowMapper);
 		return emps;
 	}
 	
