a98ac3137572d0dcba389a3b5c8b90a3cf4ec7cb
teacher <teacher@BE-403->
2022-07-11 19:28:20 +0800

    s

--- src/main/java/com/spring/core/session06/SpringJDBCConfig.java
+++ src/main/java/com/spring/core/session06/SpringJDBCConfig.java
@@ -3,10 +3,14 @@ package com.spring.core.session06;
 import javax.sql.DataSource;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.PropertySource;
 import org.springframework.core.env.Environment;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
+import org.springframework.jdbc.datasource.DataSourceTransactionManager;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 import com.mchange.v2.c3p0.ComboPooledDataSource;
@@ -22,6 +26,7 @@ public class SpringJDBCConfig {
 	@Autowired
 	protected Environment env;
 	
+	@Bean
 	public DataSource dataSource() {
 		ComboPooledDataSource ds = new ComboPooledDataSource();
 		try {
@@ -41,6 +46,21 @@ public class SpringJDBCConfig {
 			e.printStackTrace();
 		}
 		return ds;
+	}
+	
+	@Bean
+	public JdbcTemplate jdbcTemplate() {
+		return new JdbcTemplate(dataSource());
+	}
+	
+	@Bean
+	public NamedParameterJdbcTemplate namedParameterJdbcTemplate() {
+		return new NamedParameterJdbcTemplate(dataSource());
+	}
+	
+	@Bean
+	public DataSourceTransactionManager transactionManager() {
+		return new DataSourceTransactionManager(dataSource());
 	}
 	
 }
