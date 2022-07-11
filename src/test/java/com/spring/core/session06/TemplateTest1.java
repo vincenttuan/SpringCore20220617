35427d57cb0316e59491d9a6e17cad054794da72
teacher <teacher@BE-403->
2022-07-11 19:55:27 +0800

    s

--- src/test/java/com/spring/core/session06/TemplateTest1.java
+++ src/test/java/com/spring/core/session06/TemplateTest1.java
@@ -2,6 +2,7 @@ package com.spring.core.session06;
 
 import org.junit.Test;
 import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 import com.spring.core.session06.template.EmpDao;
@@ -9,7 +10,10 @@ import com.spring.core.session06.template.EmpDao;
 public class TemplateTest1 {
 	@Test
 	public void test() {
-		ApplicationContext ctx = new ClassPathXmlApplicationContext("jdbc-config.xml");
+		// 使用 xml 配置
+		//ApplicationContext ctx = new ClassPathXmlApplicationContext("jdbc-config.xml");
+		// 使用 java 配置
+		ApplicationContext ctx = new AnnotationConfigApplicationContext(SpringJDBCConfig.class);
 		
 		EmpDao empDao = ctx.getBean("empDao", EmpDao.class);
 		System.out.println(empDao.queryAll());
