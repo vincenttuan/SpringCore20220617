53ad1556269f11a83259d4a4823dcd08910ce024
teacher <teacher@BE-403->
2022-07-11 19:25:49 +0800

    s

---
+++ src/main/java/com/spring/core/session06/SpringJDBCConfig.java
@@ -0,0 +1,46 @@
+package com.spring.core.session06;
+
+import javax.sql.DataSource;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.core.env.Environment;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
+
+import com.mchange.v2.c3p0.ComboPooledDataSource;
+
+// JDBC Java 配置
+@Configuration  // 表示此類是一個配置檔
+//@ComponentScan(basePackages = {"com.spring.core.session06"})
+@ComponentScan // 預設會掃描 SpringJDBCConfig 類的所在位置
+@PropertySource(value = {"classpath:db.properties"}, ignoreResourceNotFound = true)
+@EnableTransactionManagement
+public class SpringJDBCConfig {
+	
+	@Autowired
+	protected Environment env;
+	
+	public DataSource dataSource() {
+		ComboPooledDataSource ds = new ComboPooledDataSource();
+		try {
+			// Basic
+			ds.setDriverClass(env.getProperty("jdbc.driver"));
+			ds.setJdbcUrl(env.getProperty("jdbc.url"));
+			ds.setUser(env.getProperty("jdbc.username"));
+			ds.setPassword(env.getProperty("jdbc.password"));
+			// Options
+			ds.setMinPoolSize(10);
+			ds.setMaxPoolSize(100);
+			ds.setInitialPoolSize(10);
+			ds.setMaxIdleTime(1800);
+			ds.setMaxStatements(100);
+			
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		return ds;
+	}
+	
+}
