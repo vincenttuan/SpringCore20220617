268dd9f3d2519f2ba1fc54482a27583636da647d
teacher <teacher@BE-403->
2022-07-15 21:43:57 +0800

    s

--- src/main/java/com/spring/core/session07/tx/service/BookService.java
+++ src/main/java/com/spring/core/session07/tx/service/BookService.java
@@ -5,5 +5,5 @@ import com.spring.core.session07.tx.exception.InsufficientQuantity;
 
 public interface BookService {
 	void buyOne(Integer wid, Integer bid) throws InsufficientAmount, InsufficientQuantity;
-	void buyMany(Integer wid, Integer... bid) throws InsufficientAmount, InsufficientQuantity;
+	void buyMany(Integer wid, Integer... bids) throws InsufficientAmount, InsufficientQuantity;
 }
