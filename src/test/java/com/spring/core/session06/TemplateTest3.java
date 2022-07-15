aed2f151e516a7ca4f8c2b5485ab311cda67fb88
teacher <teacher@BE-403->
2022-07-15 19:13:49 +0800

    s

--- src/test/java/com/spring/core/session06/TemplateTest3.java
+++ src/test/java/com/spring/core/session06/TemplateTest3.java
@@ -1,16 +1,9 @@
 package com.spring.core.session06;
 
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
-import java.util.Map;
-
 import org.junit.Test;
 import org.springframework.context.ApplicationContext;
-import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
-import com.spring.core.session06.entity.Emp;
 import com.spring.core.session06.template.EmpDao;
 
 public class TemplateTest3 {
