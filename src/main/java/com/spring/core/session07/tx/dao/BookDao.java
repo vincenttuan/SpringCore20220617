10b0e838649a40241c2020a22bb7f42af13aae66
teacher <teacher@BE-403->
2022-07-15 21:16:15 +0800

    s

--- src/main/java/com/spring/core/session07/tx/dao/BookDao.java
+++ src/main/java/com/spring/core/session07/tx/dao/BookDao.java
@@ -1,5 +1,12 @@
 package com.spring.core.session07.tx.dao;
 
+import com.spring.core.session07.tx.exception.InsufficientAmount;
+import com.spring.core.session07.tx.exception.InsufficientQuantity;
+
 public interface BookDao {
-	
+	Integer getPrice(Integer bid); // 根據 bid 取得書本價格
+	Integer getStockAmount(Integer bid);  // 根據 bid 取得庫存數量
+	Integer getWalletMoney(Integer wid);  // 根據 wid 取得可用餘額
+	Integer updateStock(Integer bid, Integer amount) throws InsufficientQuantity; // 減去庫存
+	Integer updateWallet(Integer wid, Integer money) throws InsufficientAmount; // 減去餘額
 }
