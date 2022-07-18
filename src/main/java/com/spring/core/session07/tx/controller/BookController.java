96cc70f712b4733cc560d9ab3e32d628b67e1930
teacher <teacher@BE-403->
2022-07-18 19:05:22 +0800

    s

--- src/main/java/com/spring/core/session07/tx/controller/BookController.java
+++ src/main/java/com/spring/core/session07/tx/controller/BookController.java
@@ -4,6 +4,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 
 import com.spring.core.session07.tx.service.BookService;
+import com.spring.core.session07.tx.service.ManyBookService;
 
 @Controller
 public class BookController {
@@ -11,12 +12,17 @@ public class BookController {
 	@Autowired
 	private BookService bookService;
 	
+	@Autowired
+	private ManyBookService manyBookService;
+	
 	public void buyBook(Integer wid, Integer bid) {
-		// 星期一寫
+		bookService.buyOne(wid, bid);
+		System.out.println("buy one ok!");
 	}
 	
 	public void buyBooks(Integer wid, Integer... bids) {
-		// 星期一寫
+		manyBookService.buyMany(wid, bids);
+		System.out.println("buy many ok!");
 	}
 	
 }
