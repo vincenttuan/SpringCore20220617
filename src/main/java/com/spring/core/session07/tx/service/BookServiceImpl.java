3cfb3ae3ffbe1e44f083e5b0efb4a457ef8d9dbf
teacher <teacher@BE-403->
2022-07-18 20:39:25 +0800

    s

--- src/main/java/com/spring/core/session07/tx/service/BookServiceImpl.java
+++ src/main/java/com/spring/core/session07/tx/service/BookServiceImpl.java
@@ -20,7 +20,12 @@ public class BookServiceImpl implements BookService {
 	 * Propagation.REQUIRED (預設):如果有事務在運行, 當前方法就在該事物中運行
 	 *                            否則就啟動一個新的事務, 並在自己的事務中運行  
 	 * Propagation.REQUIRES_NEW : 當前方法必須啟動新的事務, 並在自己的事務內運行, 
-	 *                            如果之前有事務正在運行, 就會將它掛起不用                             
+	 *                            如果之前有事務正在運行, 就會將它掛起不用
+	 * Propagation.SUPPORTS : 如果有事務在運行, 當前方法就在此事務中運行, 否則就不再事務中運行
+	 * Propagation.NOTSUPPORTED : 當前方法不應該在事務中運行, 如果有事務就會將它掛起不用
+	 * Propagation.MANDATORY : 當前方法必須在運行在事務中, 若沒有就拋出異常
+	 * Propagation.NEVER : 當前方法不應該運行在事務中, 若在事務中就拋出異常
+	 * Propagation.NESTED : 如果有事務在運行, 當前方法必須嵌套在事務中, 否則就啟動一個新事務並在自己的事務中運行
 	 * */
 	@Override
 	public void buyOne(Integer wid, Integer bid) throws InsufficientAmount, InsufficientQuantity {
